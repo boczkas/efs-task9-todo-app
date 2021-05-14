39f09fe7e434af1d730e06ffa9539ff5dd4cec1e
pawel_labaj <pawel_labaj@epam.com>
2021-05-14 21:44:18 +0200

    Jar with updated tests

--- buildTestJar.sh
+++ buildTestJar.sh
@@ -7,11 +7,12 @@ mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file \
   -Dpackaging=test-jar \
   -DcreateChecksum=true \
   -DlocalRepositoryPath=./docs/repo
+  -DgeneratePom=true
 
 mv ./docs/repo/efs/efs-task9-todo-app.tests/maven-metadata-local.xml ./docs/repo/efs/efs-task9-todo-app.tests/maven-metadata.xml
 mv ./docs/repo/efs/efs-task9-todo-app.tests/maven-metadata-local.xml.md5 ./docs/repo/efs/efs-task9-todo-app.tests/maven-metadata.xml.md5
 mv ./docs/repo/efs/efs-task9-todo-app.tests/maven-metadata-local.xml.sha1 ./docs/repo/efs/efs-task9-todo-app.tests/maven-metadata.xml.sha1
 
-mv ./docs/repo/efs/efs-task9-todo-app.tests/1.1.0-SNAPSHOT/maven-metadata-local.xml ./docs/repo/efs/efs-task9-todo-app.tests/1.1.0-SNAPSHOT/maven-metadata.xml
-mv ./docs/repo/efs/efs-task9-todo-app.tests/1.1.0-SNAPSHOT/maven-metadata-local.xml.md5 ./docs/repo/efs/efs-task9-todo-app.tests/1.1.0-SNAPSHOT/maven-metadata.xml.md5
-mv ./docs/repo/efs/efs-task9-todo-app.tests/1.1.0-SNAPSHOT/maven-metadata-local.xml.sha1 ./docs/repo/efs/efs-task9-todo-app.tests/1.1.0-SNAPSHOT/maven-metadata.xml.sha1
+rm ./docs/repo/efs/efs-task9-todo-app.tests/1.1.0-SNAPSHOT/maven-metadata-local.xml
+rm ./docs/repo/efs/efs-task9-todo-app.tests/1.1.0-SNAPSHOT/maven-metadata-local.xml.md5
+rm ./docs/repo/efs/efs-task9-todo-app.tests/1.1.0-SNAPSHOT/maven-metadata-local.xml.sha1
